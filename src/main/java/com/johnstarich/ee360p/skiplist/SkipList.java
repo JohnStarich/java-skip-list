1f5c02a5c766180210730a5274a02abd8f9dd760
John Starich <john.starich@thirdship.com>
2017-04-12 01:17:37 -0500

    Add initial Maven, code, and tests

---
+++ src/main/java/com/johnstarich/ee360p/skiplist/SkipList.java
@@ -0,0 +1,8 @@
+package com.johnstarich.ee360p.skiplist;
+
+/**
+ * A fine-grained and lock-free skip-list implementation.
+ * Created by johnstarich on 4/12/17.
+ */
+public class SkipList {
+}
---
+++ src/test/java/com/johnstarich/ee360p/skiplist/SkipListTest.java
@@ -0,0 +1,16 @@
+package com.johnstarich.ee360p.skiplist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for SkipList.
+ * Created by johnstarich on 4/12/17.
+ */
+public class SkipListTest {
+    @Test
+    public void construct() {
+        assertNotNull(new SkipList());
+    }
+}
