35565b8397a4be880d4dfa0eee7c386031086f9d
Jacob Ingalls <jacob.ingalls@thirdship.com>
2017-04-17 11:25:11 -0500

    Add super simple test

---
+++ src/test/java/com/johnstarich/ee360p/skiplist/JavaConcurrentSkipListSetTest.java
@@ -0,0 +1,12 @@
+package com.johnstarich.ee360p.skiplist;
+
+import org.junit.Test;
+
+public class JavaConcurrentSkipListSetTest {
+
+    @Test
+    public void helloTest() throws Exception {
+        System.out.print("こんにちわ");
+    }
+
+}
